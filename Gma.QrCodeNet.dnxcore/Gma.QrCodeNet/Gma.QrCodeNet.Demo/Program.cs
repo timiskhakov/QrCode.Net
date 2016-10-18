1e952698b1983a9f4ba2d5c39c6acbf4d191514d
veter <lianggx@foxmail.com>
2016-10-18 13:38:33 +0800

    Add QrCode version for dotnet core

---
+++ Gma.QrCodeNet.dnxcore/Gma.QrCodeNet/Gma.QrCodeNet.Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gma.QrCodeNet.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UsageSamples.RunSample3();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gma.QrCodeNet.dnxcore/Gma.QrCodeNet/Gma.QrCodeNet.Encoding/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gma.QrCodeNet.Encoding")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01ad80d7-98c2-42a8-a6fd-01f284dba1e0")]
